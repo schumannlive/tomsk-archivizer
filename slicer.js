be9af265f5da41e6a0ae87252d3cabf45a4adc82
Michael <82292036+schumannlive@users.noreply.github.com>
2022-07-13 08:45:38 +0200

    Update slicer.js

--- slicer.js
+++ slicer.js
@@ -4,15 +4,15 @@ const imageToSlices = require('image-to-slices');
 
 function slicepic(){
 
-    var dateOffset = 2 //2 days
+    var dateOffset = 1
     var today = new Date();
     var dd = String(today.getDate() - dateOffset).padStart(2, '');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
     var yyyy = today.getFullYear();
     today = dd + '.' + mm + '.' + yyyy;
-    
+
     var lineXArray = [1000, 1000];
-    var lineYArray = [60, 541];
+    var lineYArray = [541, 1022];
     var source = "./schumann_archive/" + today + ".jpg";
 
      imageToSlices(source, lineXArray, lineYArray, {
